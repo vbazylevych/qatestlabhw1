f096af8f8b705e9e39273f002c60911af413bd90
vbazylevych <vbazylevych@playtika.com.ua>
2018-10-26 18:31:51 +0300

    ggg

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,23 +1,31 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
 
 public class Test {
 
     static WebDriver driver;
-    LoginPage loginPage;
+    private LoginPage loginPage;
 
     @BeforeMethod
-
     public void beforeMethod() {
-
+        System.setProperty("webdriver.chrome.driver", "D:\\ddd\\src\\main\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "D:\\ddd\\src\\main\\resources\\geckodriver.exe");
         driver = new FirefoxDriver();
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
 
-        driver.get("http://www.store.demoqa.com");
+        loginPage = PageFactory.initElements(driver, LoginPage.class);
+    }
 
-        HomePage = PageFactory.initElements(driver, Home_PG_POF.class);
+    @org.testng.annotations.Test
+    public void test() {
 
-        LoginPage = PageFactory.initElements(driver, LogIn_PG_POF.class);
+        loginPage.login(" webinar.test@gmail.com", "Xcg7299bnSmMuRLp9ITw");
 
     }
 }
